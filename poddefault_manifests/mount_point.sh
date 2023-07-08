e37d287f330053ff49917602b533f1c82ccd7e8a
Mohsin A Shaikh <mohsinshaikh786@gmail.com>
2023-07-08 08:46:19 +0000

    refactoring

--- poddefault_manifests/mount_point.sh
+++ poddefault_manifests/mount_point.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
+
+APPS=
+REFERENCE='10.10.10.221'
+WORKSPACE='10.10.10.152'
+SW='10.10.10.171'
+
+
+
+#mkdir -p /mnt/apps
 mkdir -p /mnt/workspace
-mkdir -p /mnt/apps
 mkdir -p /mnt/reference
 mkdir -p /mnt/sw
 
 
-mount 10.10.10.157:/workspace /mnt/workspace
-mount 10.10.10.190:/sw /mnt/sw
-mount 10.10.10.207:/apps /mnt/apps
-mount 10.10.10.211:/reference /mnt/reference
+#mount 10.10.10.207:/apps /mnt/apps
+mount ${REFERENCE}:/reference /mnt/reference
+mount ${WORKSPACE}:/workspace /mnt/workspace
+mount ${SW}:/sw /mnt/sw
